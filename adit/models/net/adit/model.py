f783f9097c460b5b0a0565e86ee4b5e5c16e3a37
Sioban Nieradzik-Kozic <sioban.nieradzik-kozic@etu.minesparis.psl.eu>
2026-08-30 09:25:54 +0200

    correction model: generate token coord

--- adit/models/net/adit/model.py
+++ adit/models/net/adit/model.py
@@ -85,6 +85,8 @@ class ADiT(nn.Module):
         num_atoms = batch["atom_mask"].sum(-1)[token_mask].int()
         atom2token = torch.arange(num_tokens.sum(), device=device).repeat_interleave(num_atoms)
 
+        token_type = batch["token_type"]
+        atom_positions = batch["atom_positions"][token_mask]    # (L, 37, 3)
         token_coordinates = generate_token_coordinates(atom_coordinates, atom2token, atom_positions, atom_mask, token_mask, token_type, T=self.token_coord_type)
 
         if self.atom_neighbour_radius > 0.0:
@@ -93,8 +95,6 @@ class ADiT(nn.Module):
         else:
             euclidian_atom_edges, euclidian_atom_edge_feat = None, None
         if self.token_neighbour_radius > 0.0:
-            atom_positions = batch["atom_positions"][token_mask]    # (L, 37, 3)
-            token_type = batch["token_type"]
             euclidian_token_edges, euclidian_token_edge_feat = generate_euclidian_edge_index(num_tokens, token_coordinates, self.token_neighbour_radius)
         else:
             euclidian_token_edges, euclidian_token_edge_feat = None, None
